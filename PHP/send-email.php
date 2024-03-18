fe0bb7c39f2ce28328c6d8399759c95fa615b7aa
Slengm4n <99735480+Slengm4n@users.noreply.github.com>
2024-03-17 22:52:04 -0300

    Update send-email.php

--- PHP/send-email.php
+++ PHP/send-email.php
@@ -5,7 +5,7 @@ $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-require "../vendor/autoload.php";
+require '../vendor/autoload.php';
 
 use \PHPMailer\PHPMailer\PHPMailer;
 use \PHPMailer\PHPMailer\SMTP;
